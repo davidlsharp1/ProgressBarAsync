09d771085a1c2062cf4a96e8d9a9bb2d72359532
davidlsharp1 <davidlsharp1@users.noreply.github.com>
2018-08-04 13:02:51 -0500

    Update Logic.cs

--- BusinessLogic/Logic.cs
+++ BusinessLogic/Logic.cs
@@ -11,6 +11,9 @@ namespace BusinessLogic
 
         public static async Task DoStuff(IProgress<ProgressModel> progress)
         {
+            // this is my custom model created to hold information about what is happening.
+            // I will new one up, add the info to the reporters and send that object back to the view later
+            // using the progress.Report method below
             var report = new ProgressModel();
 
             for (int i = 0; i <= 10; i++)
